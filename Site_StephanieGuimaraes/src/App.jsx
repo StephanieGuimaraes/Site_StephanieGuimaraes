425969ab0cb272738b78ad7597b596997c269f88
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2024-07-16 20:57:18 -0300

    feat: using the title component

--- Site_StephanieGuimaraes/src/App.jsx
+++ Site_StephanieGuimaraes/src/App.jsx
@@ -13,6 +13,7 @@ const App = () => {
       <div className='container'>
         <TitlePages title='Publications'/>
         <Publications/>
+        <TitlePages title='Work Projects'/>
         <Sites/>
       </div>
     </div>

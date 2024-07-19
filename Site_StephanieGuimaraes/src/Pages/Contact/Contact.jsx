c07f9c4470f6d74eb529ba3d5cc87e68b00caa22
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2024-07-19 18:00:30 -0300

    feat: adding the title component to the contact page

--- Site_StephanieGuimaraes/src/Pages/Contact/Contact.jsx
+++ Site_StephanieGuimaraes/src/Pages/Contact/Contact.jsx
@@ -1,16 +1,20 @@
+import TitlePages from '../../Components/TitlePages/TitlePages'
 import './Contact.css'
 
 const Contact = () => {
   return (
-    <div className='contact'>
-        <div className='contact-col'>
-            <h3>Send us a message</h3>
-            <p>Feel free to reach out Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem cum eaque vero, sint voluptas sequi?</p>
-            <ul>
-                <li>Contact@GreatStack.dev</li>
-                <li>+55 62 9 8214 9782</li>
-                <li>Brazil, Goiânia/Go</li>
-            </ul>
+    <div>
+        <TitlePages title='Contato'/>
+        <div className='contact'>
+            <div className='contact-col'>
+                <h3>Send us a message</h3>
+                <p>Feel free to reach out Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem cum eaque vero, sint voluptas sequi?</p>
+                <ul>
+                    <li>Contact@GreatStack.dev</li>
+                    <li>+55 62 9 8214 9782</li>
+                    <li>Brazil, Goiânia/Go</li>
+                </ul>
+            </div>
         </div>
     </div>
   )

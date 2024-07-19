dd08d9f28d9408f8983bfb192c979453c0c3ae4c
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2024-07-18 23:31:22 -0300

    feat: delete compenent that is repeating

--- Site_StephanieGuimaraes/src/App.jsx
+++ Site_StephanieGuimaraes/src/App.jsx
@@ -3,7 +3,6 @@ import Hero from './Components/Hero/Hero'
 import Navbar from './Components/Navbar/Navbar'
 import Publications from './Components/Publications/Publications'
 import Sites from './Components/Sites/Sites'
-import TitlePages from './Components/TitlePages/TitlePages'
 
 const App = () => {
   return (
@@ -11,9 +10,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <div className='container'>
-        <TitlePages title='Publications'/>
         <Publications/>
-        <TitlePages title='Work Projects'/>
         <Sites/>
       </div>
     </div>

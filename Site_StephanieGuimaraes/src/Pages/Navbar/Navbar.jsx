f7506770e0d0a17059f3ac49454e33af9513869c
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2024-07-19 17:52:34 -0300

    fix: making the changing in the nav bigger

--- Site_StephanieGuimaraes/src/Pages/Navbar/Navbar.jsx
+++ Site_StephanieGuimaraes/src/Pages/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   useEffect(()=>{
     window.addEventListener('scroll', () => {
-      window.scrollY > 400 ? setSticky(true) : setSticky(false)
+      window.scrollY > 500 ? setSticky(true) : setSticky(false)
     })
   },[]);
 

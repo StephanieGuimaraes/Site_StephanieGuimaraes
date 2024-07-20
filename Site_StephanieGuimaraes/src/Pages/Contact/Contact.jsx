97107525e75aa21305a55f9515a21973175fda83
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2024-07-20 16:02:22 -0300

    feat: adding the icons and the begining of the form for messages

--- Site_StephanieGuimaraes/src/Pages/Contact/Contact.jsx
+++ Site_StephanieGuimaraes/src/Pages/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import { Email} from '@mui/icons-material'
+import { ArrowForward, Email, LocalPhone, LocationOn, MailOutline} from '@mui/icons-material'
 import TitlePages from '../../Components/TitlePages/TitlePages'
 import './Contact.css'
 
@@ -15,10 +15,21 @@ const Contact = () => {
                 </div>
                 <p>Feel free to reach out Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem cum eaque vero, sint voluptas sequi?</p>
                 <ul>
-                    <li>Contact@GreatStack.dev</li>
-                    <li>+55 62 9 8214 9782</li>
-                    <li>Brazil, Goiânia/Go</li>
+                    <li><MailOutline sx={{marginRight: "8px"}}/> Contact@GreatStack.dev</li>
+                    <li><LocalPhone sx={{marginRight: "8px"}}/> +55 62 9 8214 9782</li>
+                    <li><LocationOn sx={{marginRight: "8px"}}/> Brazil, Goiânia/Go</li>
                 </ul>
+            </div>
+            <div className='contact-col'>
+                <form>
+                  <label>Your name</label>
+                  <input type="text" name='name' placeholder='Enter your name' required/>
+                  <label>Phone Number</label>
+                  <input type="tel" name='phone' placeholder='Enter your mobile number'/>
+                  <label>Write your messages here</label>
+                  <textarea name="message" rows="6" placeholder='Enter your message' required></textarea>
+                  <button type='submit' className='btn'>Submit now <ArrowForward sx={{marginLeft:"8px"}}/> </button>
+                </form>
             </div>
         </div>
     </div>

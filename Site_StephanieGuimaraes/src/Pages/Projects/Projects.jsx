d1dbd93187bd3f584acedad28c4ffa5773a1def8
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2024-07-23 18:26:46 -0300

    feat: changing the name of the page of Site to Projects

--- Site_StephanieGuimaraes/src/Pages/Projects/Projects.jsx
+++ Site_StephanieGuimaraes/src/Pages/Projects/Projects.jsx
@@ -1,20 +1,20 @@
 //import React from 'react'
-import './Sites.css'
+import './Projects.css'
 //import PortFolio1 from '../../assets/PortFolio1.png'
 import FotoProvisoria from '../../assets/FotoProvisoria.jpg'
 import TitlePages from '../../Components/TitlePages/TitlePages'
 
-const Sites = () => {
+const Projects = () => {
   return (
     <div>
-      <TitlePages title='Work Projects'/>
-      <div className='site'>
-        <div className='site-left'>
+      <TitlePages title='Projetos'/>
+      <div className='projects'>
+        <div className='projects-left'>
           {/* <img src={PortFolio1} alt='' className='PortFolio1'/> */}
           <img src={FotoProvisoria} alt='' className='site-img1'/>
           <img src={FotoProvisoria} alt='' className='site-img1'/>
         </div>
-        <div className='site-right'>
+        <div className='projects-right'>
           <h3>About university</h3>
           <h2>Lorem, ipsum dolor sit amet consectetur.</h2>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit perspiciatis cum dicta ipsum tempore eligendi.</p>
@@ -24,4 +24,4 @@ const Sites = () => {
   )
 }
 
-export default Sites
+export default Projects

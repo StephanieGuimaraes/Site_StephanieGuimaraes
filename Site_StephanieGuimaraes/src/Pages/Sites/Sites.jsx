9827045f5672618ccb65b2673496761413faafd4
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2024-07-25 22:02:09 -0300

    feat: geting the mold for the page

--- Site_StephanieGuimaraes/src/Pages/Sites/Sites.jsx
+++ Site_StephanieGuimaraes/src/Pages/Sites/Sites.jsx
@@ -1,9 +1,24 @@
 //import React from 'react'
+import TitlePages from '../../Components/TitlePages/TitlePages'
+import FotoProvisoria from '../../assets/FotoProvisoria.jpg'
 import './Sites.css'
 
 const Sites = () => {
   return (
-    <div>Sites</div>
+    <div>
+    <TitlePages title='Sites'/>
+    <div className='sites'>
+        <div className='site'>
+            <img src={FotoProvisoria} alt=''/>
+        </div>
+        <div className='site'>
+            <img src={FotoProvisoria} alt=''/>
+        </div>
+        <div className='site'>
+            <img src={FotoProvisoria} alt=''/>
+        </div>
+    </div>
+  </div>
   )
 }
 

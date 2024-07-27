1ad3ef71415b978eeb968c2ce5ec80d2c6a02508
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2024-07-27 17:53:28 -0300

    feat: begining of the style on SimpleCard

--- Site_StephanieGuimaraes/src/Components/Cards/SimpleCard.jsx
+++ Site_StephanieGuimaraes/src/Components/Cards/SimpleCard.jsx
@@ -1,16 +1,26 @@
 //import React from 'react'
-import { Card, CardContent} from '@mui/material'
+import { Card, CardContent, Stack} from '@mui/material'
 import  './SimpleCard.css'
+import { CheckCircleOutline } from '@mui/icons-material'
 const SimpleCard = () => {
   return (
-    <Card sx={{minWidth: 275}}>
+    <Card sx={{minWidth: 275, padding: "24px"}}>
         <CardContent>
             <h3>Meu card</h3>
             <p>txt do meu card</p>
             <ul>
-                <li>Header</li>
-                <li>4 seções</li>
-                <li>Footer</li>
+                <Stack direction="row" spacing={1}>
+                    <CheckCircleOutline sx={{ color: "#b92118" }}/>
+                    <li>Header</li>
+                </Stack>
+                <Stack direction="row" spacing={1}>
+                    <CheckCircleOutline sx={{ color: "#b92118" }}/>
+                    <li>4 seções</li>
+                </Stack>
+                <Stack direction="row" spacing={1}>
+                    <CheckCircleOutline sx={{ color: "#e64319" }}/>
+                    <li>Footer</li>
+                </Stack>
             </ul>
         </CardContent>
     </Card>

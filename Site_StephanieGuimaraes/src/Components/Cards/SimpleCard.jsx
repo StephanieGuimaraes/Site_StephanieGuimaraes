2291cf90843bbcd0ac56139059d3986d7e5444ee
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2024-07-29 17:43:57 -0300

    feat: using the global variables

--- Site_StephanieGuimaraes/src/Components/Cards/SimpleCard.jsx
+++ Site_StephanieGuimaraes/src/Components/Cards/SimpleCard.jsx
@@ -9,16 +9,16 @@ const SimpleCard = () => {
             <h3>Meu card</h3>
             <p>txt do meu card</p>
             <ul>
-                <Stack direction="row" spacing={1}>
-                    <CheckCircleOutline sx={{ color: "#b92118" }}/>
+                <Stack direction="row" spacing={1} alignItems="center">
+                    <CheckCircleOutline sx={{ color: "var(--primary-100)" }}/>
                     <li>Header</li>
                 </Stack>
-                <Stack direction="row" spacing={1}>
-                    <CheckCircleOutline sx={{ color: "#b92118" }}/>
+                <Stack direction="row" spacing={1} alignItems="center">
+                    <CheckCircleOutline sx={{ color: "var(--primary-100)" }}/>
                     <li>4 seções</li>
                 </Stack>
-                <Stack direction="row" spacing={1}>
-                    <CheckCircleOutline sx={{ color: "#e64319" }}/>
+                <Stack direction="row" spacing={1} alignItems="center">
+                    <CheckCircleOutline sx={{ color: "var(--primary-100)" }}/>
                     <li>Footer</li>
                 </Stack>
             </ul>

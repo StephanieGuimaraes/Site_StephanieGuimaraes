23c36ff9daedfee842e054721f4d53181de2b346
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2024-07-29 15:01:33 -0300

    fix: changing the hardcode for the global variables

--- Site_StephanieGuimaraes/src/Pages/Navbar/Navbar.jsx
+++ Site_StephanieGuimaraes/src/Pages/Navbar/Navbar.jsx
@@ -23,37 +23,37 @@ const Navbar = () => {
             <li>
               <Stack direction="row" alignItems="center" gap="8px">
                 <a href="https://wa.me/5562982149782" target="_blank">
-                  <WhatsApp sx={{color:"#3E9BC1", fontSize: 30}}/>    
+                  <WhatsApp sx={{color: "var(--primary-50)", fontSize: 30}}/>    
                 </a>
-                <Typography fontFamily="Poppins" fontSize="16px" fontWeight={500} color="#676767">Contato: +55(62)982149782</Typography>
+                <Typography fontSize="16px" fontWeight={400} color="#676767">Contato: +55(62)982149782</Typography>
               </Stack>
             </li>
           <div className='icons-right'>
             <li>
               <Stack direction="row" alignItems="center">
                 <a href="https://www.linkedin.com/in/stephanie-cardoso-guimaraes-319306107/" target="_blank">
-                  <LinkedIn sx={{ color: "#3E9BC1", fontSize: 30 }} />
+                  <LinkedIn sx={{ color: "var(--primary-50)", fontSize: 30 }} />
                 </a>
               </Stack>
             </li>
             <li>
               <Stack direction="row" alignItems="center">
                 <a href="https://github.com/StephanieGuimaraes" target="_blank">
-                   <GitHub sx={{color:"#3E9BC1", fontSize: 30}}/>
+                   <GitHub sx={{color:"var(--primary-50)", fontSize: 30}}/>
                 </a>
               </Stack>
             </li>
             <li>
               <Stack direction="row" alignItems="center">
                 <a href="https://www.instagram.com/steph_guimaraes/" target="_blank">
-                <Instagram sx={{color:"#3E9BC1", fontSize: 30}}/>
+                <Instagram sx={{color:"var(--primary-50)", fontSize: 30}}/>
                 </a>
               </Stack>
             </li>
             <Divider orientation='vertical' variant="middle" flexItem/>
             <li>
-              <Stack direction="row" alignItems="center" gap="8px"><MailOutline sx={{color:"#3E9BC1", fontSize: 30}}/>
-                <Typography fontFamily="Poppins" fontSize="16px" fontWeight={500} color="#676767">stephanieguimaraes7@gmail.com</Typography>
+              <Stack direction="row" alignItems="center" gap="8px"><MailOutline sx={{color:"var(--primary-50)", fontSize: 30}}/>
+                <Typography fontSize="16px" fontWeight={400} color="#676767">stephanieguimaraes7@gmail.com</Typography>
               </Stack>
             </li>
           </div>

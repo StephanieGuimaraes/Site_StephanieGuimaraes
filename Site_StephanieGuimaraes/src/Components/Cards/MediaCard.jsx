16f98702abb932c6f270264115d32fac60819700
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2024-08-01 18:24:30 -0300

    feat: bulding the body

--- Site_StephanieGuimaraes/src/Components/Cards/MediaCard.jsx
+++ Site_StephanieGuimaraes/src/Components/Cards/MediaCard.jsx
@@ -1,8 +1,29 @@
+import { Card, CardContent, Stack } from '@mui/material'
 import './MediaCard'
 
 const MediaCard = () => {
   return (
-    <div>MediaCard</div>
+    <Card sx={{minWidth: 275, padding: "24px 32px", borderRadius: "15px"}} elevation={3}>
+        <CardContent>
+            <Stack>
+                <h3>Title</h3>
+                    <p>lerem5</p>
+                    <ul>
+                        <Stack spacing={1}>
+                            <Stack direction="row" spacing={1} alignItems="center">
+                                <li >Header</li>
+                            </Stack>
+                            <Stack direction="row" spacing={1} alignItems="center">
+                                <li >4 seções</li>
+                            </Stack>
+                            <Stack direction="row" spacing={1} alignItems="center">
+                                <li>Footer</li>
+                            </Stack>
+                        </Stack>
+                    </ul>
+            </Stack>
+        </CardContent>
+    </Card>
   )
 }
 

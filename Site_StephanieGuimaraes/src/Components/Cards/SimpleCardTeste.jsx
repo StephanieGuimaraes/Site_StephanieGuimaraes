353577e32f5b5ef6742a3161750b50a58313432a
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2024-08-07 18:40:22 -0300

    feat: finalização do card com o uso de props

--- Site_StephanieGuimaraes/src/Components/Cards/SimpleCardTeste.jsx
+++ Site_StephanieGuimaraes/src/Components/Cards/SimpleCardTeste.jsx
@@ -2,44 +2,44 @@
 import { Box, Card, CardContent, Stack, Typography } from '@mui/material'
 import './SimpleCardTeste.css'
 import { BoltSharp, CheckCircleOutline } from '@mui/icons-material'
+import propTypes from 'prop-types';
 
-const SimpleCardTeste = () => {
+const SimpleCardTeste = ({tag, title, subText, price, listItems}) => {
   return (
-    <Card sx={{ maxWidth: 345, borderRadius: "15px"}} elevation={4}>
+    <Card sx={{ maxWidth: 345, borderRadius: "15px"}} elevation={5}>
           <CardContent >
             <Stack sx={{padding: "16px"}}>
               <Typography gutterBottom variant="h5" fontWeight={550} component="div">
-                Landing Page Simples
+                {title}
               </Typography>
-              
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over 6,000
-                species
+              <Typography variant="body2" color="text.secondary" sx={{minHeight: "60px"}}>
+                {subText}
               </Typography>
-              <Box
-                minHeight={32}
-                maxWidth={100}
-                margin="8px 0 0"
-                display="flex"
-                alignItems="center"
-                padding="0 12px"
-                bgcolor="var(--accent-50)"
-                color="var(--white)"
-                borderRadius={4}
-                alignSelf="end"
-                fontSize="18px"
-              >
-                <Stack direction="row" alignItems="center">
-                  <Typography 
-                    variant="subtitle1" 
-                    color="var(--white)"
-                    >
-                    Popular
-                  </Typography>
-                  <BoltSharp/>
-                </Stack>
-                
-              </Box>
+              {tag ? (
+                <Box
+                  minHeight={32}
+                  maxWidth={100}
+                  margin="8px 0 0"
+                  display="flex"
+                  alignItems="center"
+                  padding="0 12px"
+                  bgcolor="var(--accent-50)"
+                  color="var(--white)"
+                  borderRadius={4}
+                  alignSelf="end"
+                  fontSize="18px"
+                >
+                  <Stack direction="row" alignItems="center">
+                    <Typography 
+                      variant="subtitle1" 
+                      color="var(--white)"
+                      >
+                      Popular
+                    </Typography>
+                    <BoltSharp/>
+                  </Stack>
+                </Box>
+              ) : ("")}
               <Stack direction="row">
                 <Typography 
                   gutterBottom variant="h5" 
@@ -56,27 +56,36 @@ const SimpleCardTeste = () => {
                   margin="16px 0 0" 
                   color="var(--primary-100)"
                   >
-                  1999
+                  {price}
                 </Typography>
               </Stack>
-              <Stack spacing={1} sx={{marginTop: "16px"}}>
-                <Stack direction="row" spacing={1} alignItems="center">
-                    <CheckCircleOutline sx={{ color: "var(--primary-100)" }}/>
-                    <li className='textCard'>Header</li>
-                </Stack>
-                <Stack direction="row" spacing={1} alignItems="center">
-                    <CheckCircleOutline sx={{ color: "var(--primary-100)" }}/>
-                    <li className='textCard'>4 seções</li>
-                </Stack>
-                <Stack direction="row" spacing={1} alignItems="center">
-                    <CheckCircleOutline sx={{ color: "var(--primary-100)" }}/>
-                    <li className='textCard'>Footer</li>
-                </Stack>
+              <Stack spacing={1} sx={{marginTop: "16px", minHeight: "100px"}}>
+                {listItems.map((item, index) => (
+                  <Stack key={index} direction="row" spacing={1} alignItems="center">
+                    <CheckCircleOutline sx={{ color: "var(--primary-100)" }} />
+                    <li className='textCard'>
+                      <Typography 
+                        variant="subtitle1"
+                        color="var(--primary-200)"
+                      >
+                        {item}
+                      </Typography>
+                    </li>
+                  </Stack>
+                ))}
               </Stack>
             </Stack>
           </CardContent> 
     </Card>
   )
 }
+
+SimpleCardTeste.propTypes = {
+  tag: propTypes.bool.isRequired,
+  title: propTypes.string.isRequired,
+  subText: propTypes.string.isRequired,
+  price: propTypes.string.isRequired,
+  listItems: propTypes.arrayOf(propTypes.string).isRequired,
+};
 
 export default SimpleCardTeste

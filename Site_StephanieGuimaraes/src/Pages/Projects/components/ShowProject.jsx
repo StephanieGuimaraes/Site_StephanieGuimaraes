05bc78808a0700de10afa8d9b95729d34826d31a
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2024-08-29 23:30:49 -0300

    feat: continuação showComponent

--- Site_StephanieGuimaraes/src/Pages/Projects/components/ShowProject.jsx
+++ Site_StephanieGuimaraes/src/Pages/Projects/components/ShowProject.jsx
@@ -5,7 +5,7 @@ import FotoProvisoria from '../../../assets/FotoProvisoria.jpg'
 
 const ShowProject = () => {
   return (
-    <Stack direction="row" spacing={4} justifyContent="center" padding={4}>
+    <Stack direction="row" spacing={4} justifyContent="center" padding={4} sx={{backgroundColor: "red"}} width="50%" >
       <Stack alignSelf="center" width="50%">
         <Typography variant="h5" fontWeight={600}>Ola</Typography>
         <Typography variant="body2" color="text.secondary">Lorem ipsum dolor sit amet  Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae voluptatem consectetur blanditiis, sapiente rerum corporis!</Typography>
